529341bc08408d6594a537d31a20179c15e0f72c
LautaroBruno <lautarobruno225@gmail.com>
2020-05-15 20:39:13 -0300

    first commit

---
+++ precache-manifest.d97baa5abe3f6223662411d466bfff9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6b704324c1fb7da7d997c380039d58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ba1ca7f96620abd8ac5",
+    "url": "/static/css/main.b787b36b.chunk.css"
+  },
+  {
+    "revision": "f78ca09cfbf740b2224b",
+    "url": "/static/js/2.3ce0316e.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.3ce0316e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ba1ca7f96620abd8ac5",
+    "url": "/static/js/main.8898dbe8.chunk.js"
+  },
+  {
+    "revision": "a95a4b1fa0f1a981a1e4",
+    "url": "/static/js/runtime-main.36389034.js"
+  }
+]);
